85234a90b12e5e41e62cc486073ed634a8698ed5
jkdijkstra <jk.dijkstra@greenteamtwente.nl>
2017-07-03 16:50:40 +0200

    test

--- test.py
+++ test.py
@@ -21,8 +21,8 @@ def updateTime():
 
 
 
-topBar = tk.Label(window, width=0, height=0, takefocus=0, sticky=E)
-topBar.grid(row=0, column=5)
+topBar = tk.Label(window, width=0, height=0, takefocus=0)
+topBar.grid(row=0, column=5, sticky=E)
 exitButton = tk.Button(window, text='exit', command=exitProgram, height=2, width=12)
 exitButton.grid(row=1, column=0)
 
