b9c030e925133c8ac4984a66fff0d41d788c3504
jkdijkstra <jk.dijkstra@greenteamtwente.nl>
2017-07-03 18:38:01 +0200

    test

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ def updateTime():
     threading.Timer(1.0, updateTime).start()
 
 
-topBar = tk.Label(window, width=74, height=0, takefocus=0)
+topBar = tk.Label(window, width=0, height=0, takefocus=0)
 topBar.grid(row=0, columnspan=10, sticky="e")
 exitButton = tk.Button(window, text='exit', command=exitProgram, height=2, width=12)
 exitButton.grid(row=1, column=0)
